857f681ae97f3d6c5bd2f03453dde5b16ca9f89a
Яценко Андрей <andrej_vmire@mail.ru>
2018-10-19 09:58:59 +0300

    add app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Layout from './app/layouts/Layout';
+
+const page = document.getElementById('page');
+
+ReactDOM.render(
+    <Layout />,
+page);

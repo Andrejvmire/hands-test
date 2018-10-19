c06a061c3601c510e068e7ffb5fe17ed9374bb3b
Яценко Андрей <andrej_vmire@mail.ru>
2018-10-19 10:17:55 +0300

    fix input id

--- src/app/components/InputColor.js
+++ src/app/components/InputColor.js
@@ -4,12 +4,12 @@ export default class InputColor extends React.Component {
     render() {
         return (
             <div>
-                <label htmlFor="rigthDownConor" className="sr-only">{this.props.data.header}</label>
+                <label htmlFor={this.props.data.id} className="sr-only">{this.props.data.header}</label>
                 <div className="input-group mb-3 mr-sm-2">
                     <div className="input-group-prepend">
                         <div className="input-group-text">#</div>
                     </div>
-                    <input id="rigthDownConor" className="form-control" placeholder="rrggbb" defaultValue={this.props.data.color} required />
+                    <input id={this.props.data.id} className="form-control" placeholder="rrggbb" defaultValue={this.props.data.color} required />
                 </div>
             </div>
         );
